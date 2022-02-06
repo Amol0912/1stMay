38ab4447505c46dc756fdf84a52d2a128c269b32
unknown <patil.amol66655@gmail.com>
2022-02-06 10:50:54 +0530

    adding Third demo

---
+++ hgasa/src/com/jbk/switchcase/demo/Third_Demo2.java
@@ -0,0 +1,31 @@
+package com.jbk.switchcase.demo;
+
+public class Third_Demo2 {
+
+	public static void main(String[] args) 
+	{
+		
+		String s= "Amol";
+		switch (s)
+		{
+		case "Amol" : System.out.println("he is amol");
+		break;
+		case "nitin" : System.out.println("he is amol");
+		break;
+		case "joshya" : System.out.println("he is amol");
+		break;
+		case "kapil" : System.out.println("he is amol");
+		break;
+		case "sachin t" : System.out.println("he is amol");
+		break;
+		default: System.out.println("nothing");
+		
+		}
+	}
+	
+	
+	
+
+	}
+
+
