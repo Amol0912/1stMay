01a55c8509bf3bc08106df0a5ceed497e9fd24d9
unknown <patil.amol66655@gmail.com>
2022-02-06 11:21:16 +0530

    adding fourth demo

---
+++ hgasa/src/com/jbk/switchcase/demo/Fourth_Demo.java
@@ -0,0 +1,25 @@
+package com.jbk.switchcase.demo;
+
+public class Fourth_Demo {
+
+	public static void main(String[] args) {
+		
+		int a =10; 
+		switch(a){
+		case 1 :    System.out.println("one");
+		break;
+		case 2 :    System.out.println("Two");
+		break;
+		case 3 :    System.out.println("Three");
+		break;
+		case 4 :    System.out.println("four");
+		break;
+		
+		default: System.out.println("invalid number");
+		
+		
+		}
+
+	}
+
+}
